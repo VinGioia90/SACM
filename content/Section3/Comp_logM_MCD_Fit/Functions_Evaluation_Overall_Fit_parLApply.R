de2c2af3e09e0a228bb90c246c2f21c565b7c53f
VinGioia90 <gioiavincenzo.25790@gmail.com>
2023-11-24 12:41:45 +0100

    Evaluation functions

--- content/Section3/Comp_logM_MCD_Fit/Functions_Evaluation_Overall_Fit_parLApply.R
+++ content/Section3/Comp_logM_MCD_Fit/Functions_Evaluation_Overall_Fit_parLApply.R
@@ -2,20 +2,21 @@
 # Needed functions for generating the data and fit the models of Section 3.3 #
 ##############################################################################
 
-# Function for data generation. Arguments:
-#  d: dimension of the outcome
-#  n: number of observations
-#  pint: percentage of intercepts in covariance matrix modelling
-#  seed: seed for generations
-#  param - type of parametrisation, one between "mcd" and "logm"
-#
-# Output: list of
-#  data - (y,x) (matrix)
-#  Sigma - simulated vcov (list of matrix)
-#  Theta - simulated Theta (list of matrix)
-#  idxint - indices intercepts (vector)
-
-
+###################################################################
+# Function for data generation. Arguments:                        #
+#  d: dimension of the outcome                                    #
+#  n: number of observations                                      #
+#  pint: percentage of intercepts in covariance matrix modelling  #
+#  seed: seed for generations                                     #
+#  param - type of parametrisation, one between "mcd" and "logm"  #
+#                                                                 #
+# Output: list of                                                 #
+#  data: (y,x) (matrix)                                           #
+#  mu: simulated mu vectors (list of vectors)                     #
+#  Sigma: simulated vcov (list of matrix)                         #
+#  Theta: simulated Theta (list of matrix)                        #
+#  idxint: indices intercepts (vector)                            #
+###################################################################
 datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm")){
   param <- match.arg(param)
 
@@ -26,17 +27,18 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
   x$x2 <- runif(n)
   x$x3 <- runif(n)
 
-  intercepts <- sample(0 : 1, d * (d + 1)/2, prob= c( pint, 1 - pint), replace = TRUE)
+  intercepts <- sample(0 : 1, d * (d + 1)/2, prob = c( pint, 1 - pint), replace = TRUE)
 
-  ## Generating functions
+  ## Generating functions for the mean vectors and the Theta matrix
   f1m <- function(x1, m1) m1 * sin(pi * x1)
   f2m <- function(x2, m2) exp(m2 * x2)
   f3m <- function(x3, m3, m4, m5, m6) m3 * x3 ^ 11 * (m4 * (1 - x3)) ^ 6 + m5 * (m6 * x3) ^ 3 * (1 - x3) ^ 10
   fvc <- function(x1,x2,  vc0, vc1, vc2, vc3, vc4, vc5, vc6, ind){
-    vc0 + ind * (vc1 * sin(2 * pi * (x1 + vc2)) + vc3 * cos(2 * pi * (x1 + vc2))
-                 + vc4 * sin(2 * pi * (x2 + vc5)) + vc6 * cos(2 * pi * (x2 + vc5)))
+    vc0 + ind * (vc1 * sin(2 * pi * (x1 + vc2)) + vc3 * cos(2 * pi * (x1 + vc2)) + vc4 * sin(2 * pi * (x2 + vc5)) + vc6 * cos(2 * pi * (x2 + vc5)))
   }
 
+  # Setting the generating parameter values
+  # For the mean vectors
   m1 <- runif(d, 1, 3)
   m2 <- runif(d, 1, 3)
   m3 <- runif(d, 0, 0.5)
@@ -44,6 +46,7 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
   m5 <- runif(d, 9, 11)
   m6 <- runif(d, 9, 11)
 
+  # For the entries of the (reparametrised) covariance matrix, that is for genereting the entries of Theta
   vc0 <- runif(d * (d + 1)/2, -0.25, 0.25)
   vc1 <- runif(d * (d + 1)/2, -0.5, 0.5)
   vc2 <- runif(d * (d + 1)/2, -0.5, 0.5)
@@ -52,6 +55,7 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
   vc5 <- runif(d * (d + 1)/2, -1, 1)
   vc6 <- runif(d * (d + 1)/2, -0.5, 0.5)
 
+  # Generation
   mu <- list()
   Sigma <- list()
   Theta <- list()
@@ -72,19 +76,21 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
       }
     }
 
-    lpi <- c(mu[[i]],diag(Theta[[i]]), Theta[[i]][upper.tri(Theta[[i]], diag=FALSE)])
+    # Converting the generated Theta entries into the sensible lpi vector ordering
+    lpi <- c(mu[[i]], diag(Theta[[i]]), Theta[[i]][upper.tri(Theta[[i]], diag = FALSE)])
 
-
+    # MCD parametrisation: data generation
     if (param ==  "mcd" ) {
       LD <- SCM::internal()$mcd_LD(lpi, d)
       L <- LD
-      diag(L) <- rep(1,d)
+      diag(L) <- rep(1, d)
       D <- diag(LD)
       Sigma[[i]] <- crossprod(t(L) * D) # L%*%diag(D^2)%*%t(L)
       u <- mvnfast::rmvn(1, rep(0, d), diag(rep(1, d)))
       y[i,] <- t(mu[[i]] + L %*% t(u * D))
     }
 
+    # logM parametrisation: data generation
     if(param ==  "logm" ) {
       Sigma[[i]] <- SCM::internal()$logM_Sigma(lpi, d)
       C <- chol(Sigma[[i]])
@@ -93,11 +99,13 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
     }
   }
 
+  # Build the data data.frame
   colnames(y) <-  paste0("y_", 1 : d)
   X <- matrix(0, n, length(x))
   for(j in 1 : length(x)) X[,j] <-  x[[j]]
   colnames(X) <- names(x)
   data <- cbind(y, X)
+
   return(list(data = data.frame(data),
               mu = mu,
               Theta = Theta,
@@ -106,22 +114,25 @@ datagen <- function(d = 2, n = 1000, pint = 0, seed = 13, param = c("mcd", "logm
 }
 
 
-# Function for setting model formulas. Arguments;
-# d: dimension of the outcome
-# expl_mean - vector of strings for the mean vector predictors
-# expl_Theta - vector of strings for the mean vector predictors
-# idxcov_int - indices for covariance modelling via intercepts
-#
-#  Output: list containing the model formulas
+#################################################################
+# Function for setting model formulas. Arguments:               #
+# d: dimension of the outcome                                   #
+# expl_mean - vector of strings for the mean vector predictors  #
+# expl_Theta - vector of strings for the mean vector predictors #
+# idxcov_int - indices for covariance modelling via intercepts  #
+#                                                               #
+# Output: list containing the model formulas                    #
+#################################################################
 
 mformula <- function(d = 2, expl_mean, expl_Theta, idxcov_int = rep(1, d * (d + 1)/2)){
 
-  meanv_f <- paste0("y_", 1 : (d - 1), sep = "|", collapse = "")
-  meanv_l <- paste0("y_", d, "~ s(", expl_mean[1], ") + s(", expl_mean[2],") + s(", expl_mean[3],")") #not beautiful: only limited to this specific case
-  mean_foo <- formula(paste0(meanv_f, meanv_l))
+  meanv_f <- paste0("y_", 1 : (d - 1), sep = "|", collapse = "") # First d - 1 outcome included in the model formula
+  meanv_l <- paste0("y_", d, "~ s(", expl_mean[1], ") + s(", expl_mean[2],") + s(", expl_mean[3],")") # last part of the model formula (not beautiful:
+                                                                                                      # only limited to this specific case)
+  mean_foo <- formula(paste0(meanv_f, meanv_l)) # Complete mean model formula specification
 
+  # Create the Theta labels (discriminating the intercepts from the covariate-dependent case)
   labelTh <- rep(0, d * (d + 1)/2)
-
   for(j in (d + 1) : (d + d * (d + 1)/2)) labelTh[j - d] <- SCM:::labTh(d, j)
   labelTh_i <- labelTh[idxcov_int != 1]
   labelTh_x <- labelTh[idxcov_int == 1]
@@ -129,8 +140,11 @@ mformula <- function(d = 2, expl_mean, expl_Theta, idxcov_int = rep(1, d * (d + 
   lint <- sum(idxcov_int != 1)
   lnoint <- sum(idxcov_int == 1)
 
+  # f and l are meant for the first and lst part of the model formula, respectively
+  # i and x are designed for capturing the possibility to consider intercepts and covariate-dependent formulas, respectively
   covi_foo_f <- covx_foo_f <- covx_foo_l <- covi_foo_l <- c()
 
+  # Covariate-dependent case
   if( lnoint > 0){
     if(lnoint > 1) {
       covx_foo_f <- paste0(labelTh_x[1 : (lnoint - 1)], sep = "|", collapse = "")
@@ -138,6 +152,8 @@ mformula <- function(d = 2, expl_mean, expl_Theta, idxcov_int = rep(1, d * (d + 
     covx_foo_l <- paste0(labelTh_x[lnoint], "~ s(", expl_Theta[1], ") + s(", expl_Theta[2], ")") #not beautiful: only limited to this specific case
     covx_foo <- formula(paste0(covx_foo_f , covx_foo_l))
   }
+
+  # Intercept case
   if(lint > 0){
     if(lint > 1){
       covi_foo_f <- paste0(labelTh_i[1 : (lint - 1)], sep = "|", collapse = "")
@@ -146,12 +162,34 @@ mformula <- function(d = 2, expl_mean, expl_Theta, idxcov_int = rep(1, d * (d + 
     covi_foo <- formula(paste0(covi_foo_f , covi_foo_l))
   }
 
-  if( lnoint > 0 & lint == 0) cov_foo <- unlist(covx_foo)
+  # Carrying out the final Theta model formula
+  if(lnoint > 0 & lint == 0) cov_foo <- unlist(covx_foo)
   if(lnoint == 0 & lint > 0) cov_foo <- unlist(covi_foo)
   if(lnoint > 0 & lint > 0) cov_foo <- c(unlist(covx_foo), unlist(covi_foo))
 
   return(c(mean_foo, cov_foo))
 }
+
+
+#################################################################################
+# Function for (simulating data and) fitting the models. Arguments:             #
+# nobs: number of observations                                                  #
+# dgrid: grid of orucome dimension values                                       #
+# param1: parametrisation for data generation ("mcd" or "logm")                 #
+# param2: parametrisation for model fitting ("mcd" or "logm")                   #
+# expl_mean: vector of strings for the mean vector predictors                   #
+# expl_Theta: vector of strings for the mean vector predictors                  #
+# save.gam: flag for saving the gam objects or return only the predicted values #
+# blockN: number of chunking blocks                                             #
+# pint: percentage of intercepts                                                #
+#                                                                               #
+# Output: list containing                                                       #
+# time: fitting times                                                           #
+# outer: number of outer iterations                                             #
+# inner: number of inner iterations                                             #
+# fit: a gam object if save.gam = "TRUE"                                        #
+# lpi_pred: the predicted linear predictor values                               #
+#################################################################################
 
 sim_est_efs <- function(nobs, dgrid,  nrun, ncores, param1, param2 = NULL, expl_mean, expl_Theta,
                          save.gam = "TRUE", blockN = rep(1, length(dgrid)), pint = 0){
@@ -162,37 +200,38 @@ sim_est_efs <- function(nobs, dgrid,  nrun, ncores, param1, param2 = NULL, expl_
   res_sim <- function(dgrid, nobs, param1, param2,  expl_mean, expl_Theta, save.gam, blockN, pint, seq_seed){
     dss <- lapply(1 : length(dgrid), function(jj){
       out <- list()
+      # Generating data
       out$sim <- datagen(d = dgrid[jj], n = nobs, pint = pint, seed = 13 * dgrid[jj] * seq_seed, param = param1)
+      # Setting the model formula
       out$foo <-  mformula(d = dgrid[jj], expl_mean = expl_mean, expl_Theta = expl_Theta, idxcov_int = out$sim$idxint)
-      if(save.gam == "TRUE"){
+      if(save.gam == "TRUE"){ # Return a gam.object
         time <- microbenchmark(out$fit <- gam_scm(out$foo,
                                                   family = mvn_scm(d = dgrid[jj], param = param2, nb = blockN[jj]),
-                                                  optimizer= "efs",  data = as.data.frame(out$sim$data)), times = 1L)
-        out$time <- time$time
-        out$outer <- out$fit$iter
-        out$inner <- out$fit$family$getNC() - 1
-      } else {
+                                                  optimizer= "efs",  data = as.data.frame(out$sim$data)), times = 1L) #here we also saved gam fit
+        out$time <- time$time                     # Computational times
+        out$outer <- out$fit$iter                 # Outer iterations
+        out$inner <- out$fit$family$getNC() - 1   # Inner iterations
+      } else { ## Return only the predicted values
         time <- microbenchmark(fit <- gam_scm(out$foo,
                                               family=mvn_scm(d = dgrid[jj], param = param2, nb = blockN[jj]),
                                               optimizer= "efs",  data = as.data.frame(out$sim$data)), times = 1L)
-        out$time <- time$time
-        out$outer<- fit$iter
-        out$inner <- fit$family$getNC() - 1
-        out$lpi_pred <- predict(fit)  ##!! Decide whether leaving the lpi_pred (maybe better) or only return mean and varcov
-        fit$family$put_cflag(FALSE) # Return the covariances instead of the correlations
-        out$vcov_pred <- predict(fit, type = "response") ##!!! see comment above
+        out$time <- time$time                     # Computational times
+        out$outer<- fit$iter                      # Outer iterations
+        out$inner <- fit$family$getNC() - 1       # Inner iterations
+        out$lpi_pred <- predict(fit)              # Predicted linear predictors
       }
       return(out)
     })
     return(dss)
   }
 
+  # Setting parallel computing
   seq_seed <- (1:nrun)^2
 
   cl <- makePSOCKcluster(ncores)
   setDefaultCluster(cl)
   clusterExport(NULL, c("nobs", "dgrid", "param1", "param2", "expl_mean", "expl_Theta",
-                        "save.gam", "blockN", "pint", "datagen","mformula","seq_seed" ), envir = environment())#,
+                        "save.gam", "blockN", "pint", "datagen","mformula","seq_seed" ), envir = environment())
   clusterEvalQ(NULL, {
     library(SCM)
     library(microbenchmark)
