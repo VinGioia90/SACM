7536d9b68a782670356ff2cf51897343a789d504
VinGioia90 <gioiavincenzo.25790@gmail.com>
2023-11-24 12:52:09 +0100

    Plot functions

--- content/Section6/Functions_Plots_Overall_Fit.R
+++ content/Section6/Functions_Plots_Overall_Fit.R
@@ -1,88 +1,259 @@
-###########################################################
-# Needed function for visualising the computational times #
-# and the iterations of the model fit                     #
-###########################################################
-
-# This function extracts and summarise the computational times, as well as the number of inner iterations. Arguments:
-#  obj: an object including the simulation results
-#  param: parametrisation
-#  dgrid: grid of outcome vector dimensions
-#  nrun: number of evaluations
-
-fit_time <- function(obj, param = c("mcd", "logm"), dgrid = NULL, nrun){
-  if(param == "mcd") param2 <- "MCD" else param2 <- "logM"
+##############################################################################
+# Needed function for visualising the computational times and the            #
+# iterations of the model fit for comparisons under the MCD parametrisation  #
+##############################################################################
 
 
-  # Number of inner iterations
-  data_iter <- data.frame(unlist(lapply(1 : length(dgrid),
-                                        function(z) unlist(lapply(1 : nrun,
-                                                  function(x) obj[[x]]$gen[[z]]$inner)))),
-                          rep(dgrid, each = nrun))
+############################################################################
+# This function extracts and summarise the computational times. Arguments: #
+#  obj: an object including the simulation results                         #
+#  param: parametrisation (her only "mcd" is set)                          #
+#  dgrid: grid of outcome vector dimensions                                #
+#  nrun: number of evaluations                                             #
+############################################################################
 
-  data_iter <- cbind(data_iter, rep(param2, each = length(dgrid)))
-  colnames(data_iter) <- c("iter", "d", "Type")
+fit_time <- function(obj, param = "mcd", dgrid = NULL, nrun){
+  if(param == "mcd") param2 <- "MCD"
 
-  data_sum_iter <- aggregate(data_iter$iter, list(data_iter$d), FUN = mean)
-  data_sum_iter_min <- aggregate(data_iter$iter, list(data_iter$d), FUN = min)
-  data_sum_iter_max <- aggregate(data_iter$iter, list(data_iter$d), FUN = max)
 
-  out_iter <- cbind(data_sum_iter, data_sum_iter_min[,2], data_sum_iter_max[,2])
+  ###################
+  # Time in minutes #
+  ###################
+  data_time_efs <-  data.frame(unlist(lapply(1 : length(dgrid),
+                                             function(z) unlist(lapply(1 : nrun, function(x) obj[[x]]$gen[[z]]$time_efs / (1e9 * 60))))),
+                               rep(dgrid, each = nrun))
+  data_time_bfgs <-  data.frame(unlist(lapply(1 : length(dgrid),
+                                             function(z) unlist(lapply(1 : nrun, function(x) obj[[x]]$gen[[z]]$time_bfgs / (1e9 * 60))))),
+                               rep(dgrid, each = nrun))
+  data_time_bfgsinit <-  data.frame(unlist(lapply(1 : length(dgrid),
+                                              function(z) unlist(lapply(1 : nrun, function(x) obj[[x]]$gen[[z]]$time_bfgs_efs / (1e9 * 60))))),
+                                rep(dgrid, each = nrun))
+  data_time_bamlss <-  data.frame(unlist(lapply(1 : length(dgrid),
+                                                  function(z) unlist(lapply(1 : nrun, function(x) obj[[x]]$gen[[z]]$time_bamlss / (1e9 * 60))))),
+                                    rep(dgrid, each = nrun))
 
-  sum_res_iter <- as.data.frame(out_iter)
-  sum_res_iter <- cbind(sum_res_iter, rep(param2, each = length(dgrid)))
-  colnames(sum_res_iter) <- c("d", "mean_iter", "min_iter", "max_iter", "Type")
+  res <- cbind(data_time_efs[,1], data_time_bfgs[,1], data_time_bfgsinit[,1], data_time_bamlss[,1],data_time_efs[,2])
+  colnames(res) <- c("time_efs", "time_bfgs", "time_bfgsinit", "time_bamlss", "d")
+  res <- as.data.frame(res)
 
-  # Time in minutes
-  data_time <-  data.frame(unlist(lapply(1 : length(dgrid),
-                                         function(z) unlist(lapply(1 : nrun, function(x) obj[[x]]$gen[[z]]$time / (1e9 * 60))))),
-                           rep(dgrid, each = nrun))
-  res <- data_time
+  ###################
+  # Summary indices #
+  ###################
+  mean_efs_time <- aggregate(res$time_efs, list(res$d), FUN = mean)[,2]
+  mean_bfgs_time <- aggregate(res$time_bfgs, list(res$d), FUN = mean)[,2]
+  mean_bfgsinit_time <- aggregate(res$time_bfgsinit, list(res$d), FUN = mean)[,2]
+  mean_bamlss_time <- aggregate(res$time_bamlss, list(res$d), FUN = mean)[,2]
 
-  res <- cbind(res, rep(param2, each = length(dgrid)))
-  colnames(res) <- c("time", "d", "Type")
-
-  data <- aggregate(res$time, list(res$d), FUN = mean)
-  data_min <- aggregate(res$time, list(res$d), FUN = min)
-  data_max <- aggregate(res$time, list(res$d), FUN = max)
-
-  out <- cbind(data,data_min[,2], data_max[,2])
-
+  out <- cbind(mean_efs_time, mean_bfgs_time, mean_bfgsinit_time, mean_bamlss_time, dgrid)
+  colnames(out) <- c("efs", "bfgs", "bfgsinit", "bamlss", "d")
   sum_res <- as.data.frame(out)
-  sum_res <- cbind(sum_res, rep(param2, each = length(dgrid)))
-  colnames(sum_res) <- c("d", "mean_time", "min_time", "max_time", "Type")
 
   return(list(sum_res = data.frame(sum_res),
-              res = data.frame(res),
-              iter = data.frame(data_iter),
-              sum_iter = data.frame(sum_res_iter)))
+              res = data.frame(res)))
+}
+
+########################################################
+# The idx_bamlss() function allows to get the indices  #
+# of bamlss in the ordering used in our implementation #
+########################################################
+idx_bamlss <- function(d){
+  mat_idx <- matrix(0, d, d)
+  count <- 2 * d + 1
+  for(i in 1 : d){
+    mat_idx[i, i] <- i + d
+    if(i < d){
+      for(j in (i + 1) : d){
+        mat_idx[i, j] <- count
+        count <- count + 1
+      }
+    }
+  }
+  idx1 <-   c(diag(mat_idx), mat_idx[upper.tri(mat_idx, diag = FALSE)])
+  idx2 <- (d + 1) : (d + d * (d + 1)/2)
+  return(list(idx1 = idx1, idx2 = idx2) )
+}
+
+####################################################################
+# This function computes the logScore on the train set. Arguments: #
+#  obj: an object including the simulation results                 #
+#  nrun: number of evaluations                                     #
+#  dgrid: grid of outcome vector dimensions                        #
+#  nobs: number of observations (of the train set)                 #
+#  param: parametrisation (only "mcd")                             #
+####################################################################
+
+log_Score_train <- function(obj, nrun, dgrid, nobs, param = "mcd"){
+
+  res_efs <- lapply(1 : length(dgrid),
+                    function(z) lapply(1 : nrun,
+                                   function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_efs, as.matrix(obj[[x]]$gen[[z]]$sim$data_train[, c(1 : dgrid[z])]))))
+  res_bfgs <- lapply(1 : length(dgrid),
+                     function(z) lapply(1 : nrun,
+                                       function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_bfgs, as.matrix(obj[[x]]$gen[[z]]$sim$data_train[, c(1 : dgrid[z])]))))
+  res_bfgs_efs <- lapply(1 : length(dgrid),
+                         function(z) lapply(1 : nrun,
+                                        function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_bfgs_efs, as.matrix(obj[[x]]$gen[[z]]$sim$data_train[, c(1 : dgrid[z])]))))
+
+  lpi_pred_bamlss <- list()
+  for(j in 1:length(dgrid)){
+    lpi_pred_bamlss[[j]]<-list()
+    for(k in 1:nrun){
+      lpi_pred_bamlss[[j]][[k]] <- matrix(0,nobs,dgrid[j]+dgrid[j]*(dgrid[j]+1)/2)
+      idx <- idx_bamlss(dgrid[j])
+      for(l in 1:dgrid[j]){
+        lpi_pred_bamlss[[j]][[k]][,l] <-  obj[[k]]$gen[[j]]$lpi_pred_bamlss[[l]]
+      }
+      for(l in 1:(dgrid[j]*(dgrid[j]+1)/2)){
+        if(idx$idx2[l] > 2*dgrid[j]){
+          lpi_pred_bamlss[[j]][[k]][,idx$idx2[l]] <-  -obj[[k]]$gen[[j]]$lpi_pred_bamlss[[idx$idx1[l]]]
+        } else {
+          lpi_pred_bamlss[[j]][[k]][,idx$idx2[l]] <-  obj[[k]]$gen[[j]]$lpi_pred_bamlss[[idx$idx1[l]]]
+        }
+      }
+    }
+  }
+
+  res_bamlss <- lapply(1 : length(dgrid),
+                       function(z) lapply(1 : nrun,
+                                          function(x) SCM::internal()$ll_mcd(lpi_pred_bamlss[[z]][[x]], as.matrix(obj[[x]]$gen[[z]]$sim$data[, c(1 : dgrid[z])]))))
+
+  lpi_pred_gen <- list()
+  for(i in 1:length(dgrid)){
+    lpi_pred_gen[[i]] <- list()
+    for(j in 1:nrun){
+      lpi_pred_gen[[i]][[j]] <- matrix(NA, nobs, dgrid[i] + dgrid[i]*(dgrid[i] + 1)/2)
+      for(k in 1: nobs){
+        lpi_pred_gen[[i]][[j]][k, ] <- c(obj[[j]]$gen[[i]]$sim$mu_train[[k]],   diag(obj[[j]]$gen[[i]]$sim$Theta_train[[k]]),
+                                         obj[[j]]$gen[[i]]$sim$Theta_train[[k]][upper.tri(obj[[j]]$gen[[i]]$sim$Theta_train[[k]], diag=FALSE)])
+      }
+    }
+  }
+
+
+  res_gen <- lapply(1 : length(dgrid),
+                function(z) lapply(1 : nrun,
+                                   function(x) SCM::internal()$ll_mcd(lpi_pred_gen[[z]][[x]], as.matrix(obj[[x]]$gen[[z]]$sim$data_train[, c(1 : dgrid[z])]))))
+  res <- list(res_gen, res_efs, res_bfgs, res_bfgs_efs, res_bamlss)
+
+  out <- list()
+  for(i in 1 : 5){
+    out[[i]] <- matrix(0, nrun, length(dgrid))
+    for(j in 1:length(dgrid)){
+      for(k in 1:nrun){
+        out[[i]][k,j] <- -res[[i]][[j]][[k]]
+      }
+    }
+  }
+
+  return(out)
+}
+
+####################################################################
+# This function computes the logScore on the test set. Arguments:  #
+#  obj: an object including the simulation results                 #
+#  nrun: number of evaluations                                     #
+#  dgrid: grid of outcome vector dimensions                        #
+#  nobs: number of observations (of the test set)                  #
+#  param: parametrisation (only "mcd")                             #
+####################################################################
+log_Score_test <- function(obj, nrun, dgrid, nobs, param = "mcd"){
+
+  res_efs <- lapply(1 : length(dgrid),
+                    function(z) lapply(1 : nrun,
+                                       function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_test_efs, as.matrix(obj[[x]]$gen[[z]]$sim$data_test[, c(1 : dgrid[z])]))))
+  res_bfgs <- lapply(1 : length(dgrid),
+                     function(z) lapply(1 : nrun,
+                                        function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_test_bfgs, as.matrix(obj[[x]]$gen[[z]]$sim$data_test[, c(1 : dgrid[z])]))))
+  res_bfgs_efs <- lapply(1 : length(dgrid),
+                         function(z) lapply(1 : nrun,
+                                            function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred_test_bfgs_efs, as.matrix(obj[[x]]$gen[[z]]$sim$data_test[, c(1 : dgrid[z])]))))
+
+  lpi_pred_test_bamlss <- list()
+  for(j in 1:length(dgrid)){
+    lpi_pred_test_bamlss[[j]]<-list()
+    for(k in 1:nrun){
+      lpi_pred_test_bamlss[[j]][[k]] <- matrix(0,nobs,dgrid[j]+dgrid[j]*(dgrid[j]+1)/2)
+      idx <- idx_bamlss(dgrid[j])
+      for(l in 1:dgrid[j]){
+        lpi_pred_test_bamlss[[j]][[k]][,l] <-  obj[[k]]$gen[[j]]$lpi_pred_test_bamlss[[l]]
+      }
+      for(l in 1:(dgrid[j]*(dgrid[j]+1)/2)){
+        if(idx$idx2[l] > 2*dgrid[j]){
+          lpi_pred_test_bamlss[[j]][[k]][,idx$idx2[l]] <-  -obj[[k]]$gen[[j]]$lpi_pred_test_bamlss[[idx$idx1[l]]]
+        } else {
+          lpi_pred_test_bamlss[[j]][[k]][,idx$idx2[l]] <-  obj[[k]]$gen[[j]]$lpi_pred_test_bamlss[[idx$idx1[l]]]
+        }
+      }
+    }
+  }
+
+  res_bamlss <- lapply(1 : length(dgrid),
+                       function(z) lapply(1 : nrun,
+                                          function(x) SCM::internal()$ll_mcd(lpi_pred_test_bamlss[[z]][[x]], as.matrix(obj[[x]]$gen[[z]]$sim$data_test[, c(1 : dgrid[z])]))))
+
+  lpi_pred_test_gen <- list()
+  for(i in 1:length(dgrid)){
+    lpi_pred_test_gen[[i]] <- list()
+    for(j in 1:nrun){
+      lpi_pred_test_gen[[i]][[j]] <- matrix(NA, nobs, dgrid[i] + dgrid[i]*(dgrid[i] + 1)/2)
+      for(k in 1: nobs){
+        lpi_pred_test_gen[[i]][[j]][k, ] <- c(obj[[j]]$gen[[i]]$sim$mu_test[[k]],   diag(obj[[j]]$gen[[i]]$sim$Theta_test[[k]]),
+                                              obj[[j]]$gen[[i]]$sim$Theta_test[[k]][upper.tri(obj[[j]]$gen[[i]]$sim$Theta_test[[k]], diag=FALSE)])
+      }
+    }
+  }
+
+  res_gen <- lapply(1 : length(dgrid),
+                    function(z) lapply(1 : nrun,
+                                       function(x) SCM::internal()$ll_mcd(lpi_pred_test_gen[[z]][[x]], as.matrix(obj[[x]]$gen[[z]]$sim$data_test[, c(1 : dgrid[z])]))))
+  res <- list(res_gen, res_efs, res_bfgs, res_bfgs_efs, res_bamlss)
+
+  out <- list()
+  for(i in 1 : 5){
+    out[[i]] <- matrix(0, nrun, length(dgrid))
+    for(j in 1:length(dgrid)){
+      for(k in 1:nrun){
+        out[[i]][k,j] <- -res[[i]][[j]][[k]]
+      }
+    }
+  }
+
+  return(out)
 }
 
 
+##################################################################################
+# This function extract the LAML (on the train set) for efs and bfgs. Arguments: #
+#  obj: an object including the simulation results                               #
+#  nrun: number of evaluations                                                   #
+#  dgrid: grid of outcome vector dimensions                                      #
+#  nobs: number of observations (of the train set)                               #
+#  param: parametrisation (only "mcd")                                           #
+#################################################################################
 
-# This function computes the logScore. Arguments:
-#  obj: an object including the simulation results
-#  nrun: number of evaluations
-#  dgrid: grid of outcome vector dimensions
-#  nobs: number of observations
-#  param: parametrisation
+LAML_extraction <- function(obj, nrun, dgrid, nobs, param = "mcd"){
 
-log_Score <- function(obj, nrun, dgrid, nobs, param = c("logm","mcd"), ncov = NULL){
-  param <- match.arg(param)
-  if(param == "mcd"){
-    res <- lapply(1 : length(dgrid),
-                  function(z) lapply(1 : nrun,
-                                     function(x) SCM::internal()$ll_mcd(obj[[x]]$gen[[z]]$lpi_pred, as.matrix(obj[[x]]$gen[[z]]$sim$data[, c(1 : dgrid[z])]))))
-  } else {
-    res <- lapply(1 : length(dgrid),
-                  function(z) lapply(1 : nrun,
-                                     function(x) SCM::internal()$ll_logm(obj[[x]]$gen[[z]]$lpi_pred, as.matrix(obj[[x]]$gen[[z]]$sim$data[, c(1 : dgrid[z])]))))
-  }
+  res_efs <- lapply(1 : length(dgrid),
+                    function(z) lapply(1 : nrun,
+                                       function(x) sim_mcd_fit[[x]]$gen[[z]]$LAML_efs))
+  res_bfgs <- lapply(1 : length(dgrid),
+                     function(z) lapply(1 : nrun,
+                                        function(x) sim_mcd_fit[[x]]$gen[[z]]$LAML_bfgs))
+  res_bfgs_efs <- lapply(1 : length(dgrid),
+                         function(z) lapply(1 : nrun,
+                                            function(x) sim_mcd_fit[[x]]$gen[[z]]$LAML_bfgs_efs))
 
-  out <- matrix(0, nrun, length(dgrid))
-  for(j in 1:length(dgrid)){
-    for(k in 1:nrun){
-      out[k,j] <- -res[[j]][[k]]
+  res <- list(res_efs, res_bfgs, res_bfgs_efs)
+
+  out <- list()
+  for(i in 1 : 3){
+    out[[i]] <- matrix(0, nrun, length(dgrid))
+    for(j in 1:length(dgrid)){
+      for(k in 1:nrun){
+        out[[i]][k,j] <- res[[i]][[j]][[k]]
+      }
     }
   }
+
   return(out)
 }
